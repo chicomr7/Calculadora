20d0bdce8a30c6cedc1dd45e12e8150d37afc554
chicomr7 <francisco1599512@gmail.com>
2021-03-26 12:37:14 -0300

    Nova Atualização do Formulário

--- resources/views/soma.blade.php
+++ resources/views/soma.blade.php
@@ -7,16 +7,22 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
-	<style> 
+ 
+<style > 
      body{
 		font-family: 'Noto Sans JP', sans-serif;
 	 }
-
-	 
 	 
 	</style>
+
+
+
+
+
 </head>
-<body>
+
+<body >
+	<center>
  <div class="container"> 
 	<div class="row">
 		<h1>  Cabeçalho	</h1>
@@ -42,7 +48,7 @@
 		<h1>  Rodapé	</h1>
 	</div>
  </div>
-	
+</center>
     
 </body>
 </html>
