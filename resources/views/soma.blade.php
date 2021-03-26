4326233f35dd87a6652251f2b2793724babac46c
chicomr7 <francisco1599512@gmail.com>
2021-03-25 23:13:31 -0300

    Nova Atualização

--- resources/views/soma.blade.php
+++ resources/views/soma.blade.php
@@ -4,11 +4,31 @@
 	<meta charset="utf-8"/>
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
 	<title>Calculadora</title>
-	
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+	<style> 
+     body{
+		font-family: 'Noto Sans JP', sans-serif;
+	 }
+
+	 
+	 
+	</style>
 </head>
 <body>
- 
-	Calculadora
+ <div class="container"> 
+	<div class="row">
+		<h1>  Cabeçalho	</h1>
+	</div>
+	<div class="row">
+		<h1>  Calculadora	</h1>
+	</div>
+	<div class="row">
+		<h1>  Rodapé	</h1>
+	</div>
+ </div>
+	
     
 </body>
 </html>
